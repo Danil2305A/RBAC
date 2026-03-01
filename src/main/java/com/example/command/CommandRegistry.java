70e14d8d4f6912269feaf3ee5e639136c535e0ed
Akusko Danil Dmitrievich <akusko.dan@yandex.ru>
2026-03-02 01:45:19 +0700

    feat: add user commands, permission commands and service commands in CommandRegistry

---
+++ src/main/java/com/example/command/CommandRegistry.java
@@ -0,0 +1,267 @@
+package com.example.command;
+
+import com.example.filter.UserFilter;
+import com.example.filter.UserFilters;
+import com.example.model.Permission;
+import com.example.model.RoleAssignment;
+import com.example.model.User;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class CommandRegistry {
+    public CommandRegistry(CommandParser parser) {
+        new UserCommands(parser);
+        new PermissionCommands(parser);
+        new ServiceCommands(parser);
+    }
+
+    private static class UserCommands {
+        public UserCommands(CommandParser parser) {
+            parser.registerCommand("user-list", "List of all users",
+                    (scanner, system) -> {
+                        if (scanner.hasNext()) {
+                            parser.executeCommand("user-search", scanner, system);
+                        } else {
+                            List<User> users = system.getUserManager().findAll();
+                            if (users.isEmpty()) {
+                                System.out.println("Missing users");
+                                return;
+                            }
+                            users.forEach(user -> System.out.println(user.format()));
+                        }
+                    });
+
+            parser.registerCommand("user-create", "Create new user",
+                    (scanner, system) -> {
+                        System.out.print("Enter username: ");
+                        String username = scanner.next();
+
+                        System.out.print("Enter fullName: ");
+                        String fullName = scanner.next();
+
+                        System.out.println("Enter email: ");
+                        String email = scanner.next();
+
+                        User newUser = null;
+                        try {
+                            newUser = User.validate(username, fullName, email);
+                        } catch (Exception e) {
+                            System.out.printf("Error creating user: %s\n", e.getMessage());
+                        }
+
+                        system.getUserManager().add(newUser);
+                        System.out.println("New user has been successfully created");
+                    });
+
+            parser.registerCommand("user-view", "Get user information",
+                    (scanner, system) -> {
+                        System.out.print("Enter username: ");
+                        String username = scanner.next();
+
+                        Optional<User> user = system.getUserManager().findByUsername(username);
+                        if (user.isEmpty()) {
+                            System.out.printf("User with username '%s' not found\n", username);
+                            return;
+                        }
+                        System.out.printf("User: %s\n", user.get().format());
+
+                        System.out.println("Assigned roles and their permissions:");
+                        List<RoleAssignment> assignments = system.getAssignmentManager().findByUser(user.get());
+                        if (assignments.isEmpty()) {
+                            System.out.println("missing");
+                            return;
+                        }
+                        assignments.forEach(assignment -> System.out.println(assignment.role()));
+                    });
+
+            parser.registerCommand("user-update", "Update user data",
+                    (scanner, system) -> {
+                        System.out.print("Enter username: ");
+                        String username = scanner.next();
+
+                        System.out.print("Enter new fullName: ");
+                        String newFullName = scanner.next();
+
+                        System.out.print("Enter new email: ");
+                        String newEmail = scanner.next();
+
+                        try {
+                            system.getUserManager().update(username, newFullName, newEmail);
+                        } catch (Exception e) {
+                            System.out.printf("Error updating user data: %s\n", e.getMessage());
+                        }
+
+                        System.out.println("User data has been successfully updated");
+                    });
+
+            parser.registerCommand("user-delete", "Delete user",
+                    (scanner, system) -> {
+                        System.out.print("Enter username: ");
+                        String username = scanner.next();
+
+                        System.out.print("Confirm user deletion? (y/n): ");
+                        String answer = scanner.next();
+                        if (!answer.equals("y".toLowerCase())) {
+                            System.out.println("Canceling user deletion");
+                            return;
+                        }
+
+                        Optional<User> user = system.getUserManager().findByUsername(username);
+                        if (user.isEmpty()) {
+                            System.out.printf("User with username '%s' not found\n", username);
+                            return;
+                        }
+
+                        system.getAssignmentManager().findByUser(user.get()).forEach(
+                                roleAssignment -> system.getAssignmentManager().remove(roleAssignment)
+                        );
+
+                        system.getUserManager().remove(user.get());
+                        System.out.println("User and his assignments have been successfully deleted");
+                    });
+
+            parser.registerCommand("user-search", "Search users by filters",
+                    (scanner, system) -> {
+                        String[] options = {"by username (contains)",
+                                "by email (contains)",
+                                "by email domain",
+                                "by full name (contains)"};
+
+                        int i = 1;
+                        for (String option : options) {
+                            System.out.println("\t" + i + ". " + option);
+                            ++i;
+                        }
+
+                        System.out.print("Select filter number (default 1) and enter filter key: ");
+                        int filterNumber = scanner.nextInt();
+                        String filterKey = "";
+                        if (scanner.hasNext()) {
+                            filterKey = scanner.next();
+                        }
+
+                        UserFilter filter = null;
+                        switch (filterNumber) {
+                            case 2 -> filter = UserFilters.byEmail(filterKey);
+                            case 3 -> filter = UserFilters.byEmailDomain(filterKey);
+                            case 4 -> filter = UserFilters.byFullNameContains(filterKey);
+                            default -> filter = UserFilters.byUsername(filterKey);
+                        }
+
+                        List<User> filteredUsers = system.getUserManager().findByFilter(filter);
+                        if (filteredUsers.isEmpty()) {
+                            System.out.println("missing");
+                            return;
+                        }
+                        System.out.println("Filtered users:");
+                        filteredUsers.forEach(user -> System.out.println(user.format()));
+                    });
+        }
+    }
+
+    private static class PermissionCommands {
+        public PermissionCommands(CommandParser parser) {
+            parser.registerCommand("permissions-user", "All permissions of specific user",
+                    (scanner, system) -> {
+                        System.out.print("Enter username: ");
+                        String username = scanner.next();
+
+                        Optional<User> user = system.getUserManager().findByUsername(username);
+                        if (user.isEmpty()) {
+                            System.out.printf("User with username '%s' not found\n", username);
+                            return;
+                        }
+
+                        List<RoleAssignment> assignments = system.getAssignmentManager().findByUser(user.get());
+                        if (assignments.isEmpty()) {
+                            System.out.printf("User with username '%s' does not have any roles\n", username);
+                            return;
+                        }
+
+                        Map<String, Set<Permission>> permissionsByResource = assignments.stream()
+                                .filter(RoleAssignment::isActive)
+                                .flatMap(roleAssignment -> roleAssignment.role().getPermissions().stream())
+                                .collect(Collectors.groupingBy(
+                                        Permission::resource,
+                                        Collectors.toCollection(HashSet::new)
+                                ));
+
+                        if (permissionsByResource.isEmpty()) {
+                            System.out.printf("User with username '%s' has no permissions\n", username);
+                            return;
+                        }
+                        permissionsByResource.forEach((resource, permissions) -> {
+                            System.out.printf("\nResource: %s\n", resource);
+                            System.out.println("  Permissions:");
+                            permissions.forEach(perm -> System.out.println(perm.format()));
+                        });
+                    });
+
+            parser.registerCommand("permissions-check", "Check if user has specific permission",
+                    (scanner, system) -> {
+                        System.out.print("Enter username: ");
+                        String username = scanner.next();
+
+                        Optional<User> user = system.getUserManager().findByUsername(username);
+                        if (user.isEmpty()) {
+                            System.out.printf("User with username '%s' not found\n", username);
+                            return;
+                        }
+
+                        System.out.print("Enter permission name: ");
+                        String permissionName = scanner.next();
+
+                        System.out.println("Enter permission resource: ");
+                        String permissionResource = scanner.next();
+
+                        boolean userHasPermission = system.getAssignmentManager().userHasPermission(user.get(),
+                                permissionName, permissionResource);
+
+                        if (!userHasPermission) {
+                            System.out.println("missing");
+                            return;
+                        }
+
+                        List<RoleAssignment> roleAssignments = system.getAssignmentManager().findByUser(user.get());
+                        System.out.print("Roles that have this permission: ");
+                        for (RoleAssignment assignment : roleAssignments) {
+                            if (assignment.role().hasPermission(permissionName, permissionResource)) {
+                                System.out.print(assignment.role().getName());
+                            }
+                        }
+                    });
+        }
+    }
+
+    private static class ServiceCommands {
+        public ServiceCommands(CommandParser parser) {
+            parser.registerCommand("help", "List of available commands",
+                    (scanner, system) -> {
+                        parser.printHelp();
+                    });
+
+            parser.registerCommand("stats", "System statistics",
+                    (scanner, system) -> {
+                        System.out.println(system.generateStatistics());
+                    });
+
+            parser.registerCommand("clear", "Clear screen",
+                    (scanner, system) -> {
+                        System.out.print("\033[H\033[3J");
+                    });
+
+            parser.registerCommand("exit", "Exiting the program",
+                    (scanner, system) -> {
+                        System.out.print("Confirm exiting the program? (y/n): ");
+                        String answer = scanner.next();
+                        if (answer.equals("y".toLowerCase())) {
+                            scanner.close();
+                            System.out.println("Bye");
+                        } else {
+                            System.out.println("Cancelling exiting the program");
+                        }
+                    });
+        }
+    }
+}
