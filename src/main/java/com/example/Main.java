25af7d9eb78aae2a179c0cc19bc407d00f0d0c62
Akusko Danil Dmitrievich <akusko.dan@yandex.ru>
2026-03-03 21:47:12 +0700

    feat: add program entrypoint

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,96 +1,29 @@
 package com.example;
 
-import com.example.model.*;
+import com.example.command.CommandParser;
+import com.example.command.CommandRegistry;
+import com.example.command.RBACSystem;
 
-import java.util.Set;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        User user1 = User.validate("  akdante", "Akusko Danil Dmitrievich", "akusko.dan@yandex.ru");
-        System.out.println(user1.format());
+        RBACSystem system = new RBACSystem();
+        system.initialize();
 
-        User user2 = User.validate(" akdante ", "Akusko Danil Dmitrievich  ", " akusko.dan@dev123.sibsutis.ru");
-        System.out.println(user2.format());
+        CommandParser parser = new CommandParser();
+        CommandRegistry.registerAllCommands(parser);
 
-        try {
-            User.validate("ak", "Akusko Danil Dmitrievich", "akusko.dan@yandex.ru");
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.print("\nEnter the command: ");
+            try {
+                String input = scanner.nextLine();
+
+                parser.parseAndExecute(input, scanner, system);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
         }
-
-        try {
-            User.validate("матушка земля", "Akusko Danil Dmitrievich", "akusko.dan@yandex.ru");
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
-
-        try {
-            User.validate("ak_dante23", "  ", "akusko.dan@yandex.ru");
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
-
-        try {
-            User.validate("akdante", null, "akusko.dan@yandex.ru");
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
-
-        try {
-            User.validate("akdante", "Akusko Danil Dmitrievich", "akusko.danyandex.ru");
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
-
-        try {
-            User.validate("akdante", "Akusko Danil Dmitrievich", "akusko.dan@yandexru");
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
-
-        System.out.println();
-
-        Permission permission = new Permission(" Read", "Users ", " Can view user list");
-        System.out.println(permission.format());
-        System.out.println(permission.matches("re", "us"));
-
-        System.out.println();
-
-        Set<Permission> permissions = Set.of(
-                permission,
-                new Permission("Write", "Users", "Can add user to list"),
-                new Permission("Delete", "Users", "Can delete user from list")
-        );
-
-        Role admin = new Role(" Administrator", "Full access ", permissions);
-        System.out.println(admin);
-        System.out.println(admin.hasPermission("re", "us"));
-        try {
-            new Role("Administrator", "Full access", permissions);
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
-
-        System.out.println();
-
-        AssignmentMetadata metadata = AssignmentMetadata.now(" Ivanov Ivan", "I don't know ");
-        System.out.println(metadata.format());
-
-        System.out.println();
-
-        PermanentAssignment permanentRole = new PermanentAssignment(user1, admin, metadata);
-        System.out.println(permanentRole.summary());
-        permanentRole.revoke();
-        System.out.println(permanentRole.summary());
-
-        System.out.println();
-
-        TemporaryAssignment temporaryRole = new TemporaryAssignment(user1, admin, metadata,
-                "2026-02-15 17:50:00 +07:00", true);
-        temporaryRole.extend("2026-02-15 21:00:00 +09:00");
-        System.out.println(temporaryRole.summary());
-        System.out.println("Is active: " + temporaryRole.isActive());
-        System.out.println("Is expired: " + temporaryRole.isExpired());
-        System.out.println("Time remaining: " + temporaryRole.getTimeRemaining());
     }
 }
