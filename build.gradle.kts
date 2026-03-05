571871f75517a9df8fbf00bf49e64c52ec0990cc
Akusko Danil Dmitrievich <akusko.dan@yandex.ru>
2026-03-06 01:46:06 +0700

    chore: add Mockito dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,9 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    testImplementation("org.mockito:mockito-core:5.22.0")
+    testImplementation("org.mockito:mockito-junit-jupiter:5.22.0")
 }
 
 tasks.test {
