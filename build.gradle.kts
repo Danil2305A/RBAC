74aced1654e9856da4b9c1dbfc0ec7c403ec9305
Akusko Danil Dmitrievich <akusko.dan@yandex.ru>
2026-03-05 21:20:39 +0700

    chore: add Jackson dependency and set shadow plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,25 +1,33 @@
 plugins {
     id("java")
+    id("application")
+    id("com.gradleup.shadow") version "8.3.0"
 }
 
 group = "com.example"
+
+application {
+    mainClass = "com.example.Main"
+}
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    implementation("com.fasterxml.jackson.core:jackson-core:2.21.1")
+    implementation("com.fasterxml.jackson.core:jackson-annotations:2.21")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.21.1")
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
-tasks.jar {
-    manifest {
-        attributes["Main-Class"] = "com.example.Main"
-    }
-}
-
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks.shadowJar {
+    archiveClassifier.set("")
 }
